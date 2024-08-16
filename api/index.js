c217534be191e2a159af0f2af5b928ed17cc5762
johan.hammarstedt@j-s-h.com <johan.hammarstedt@j-s-h.com>
2024-08-16 13:34:59 +0200

    /message

--- api/index.js
+++ api/index.js
@@ -16,7 +16,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send(
+    "Hello, please add '/products' to the url to see the database content."
+  );
 });
 
 dotenv.config();
